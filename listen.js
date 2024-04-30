97ec1b8320a5b231a2e18a864345dfc557d6f137
Sophie <sophie.v.p@hotmail.co.uk>
2024-04-30 21:57:22 +0100

    adds setup for hosting

--- listen.js
+++ listen.js
@@ -1,5 +1,4 @@
-const app = require("./app");
+const app = require("./app.js");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, () => {
-  console.log("App listening on port 9090......");
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
